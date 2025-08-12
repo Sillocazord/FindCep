6985994d6d8c0465bf848c32ce028e1610d4cc17
Sillas Endrigo <sillas.silva@aluno.senai.br>
2025-08-12 15:07:57 -0300

    :tada: Enviando projeto com a tela de consulta em desenvolvimento

---
+++ findcep/app/index.jsx
@@ -0,0 +1,59 @@
+
+import { StyleSheet, Text, View, ImageBackground, Image } from "react-native";
+import {Input} from "../components/input/input.jsx"
+
+export default function Index() {
+  return (
+    <>
+
+
+      {/*1. Logo + imagem de fundo*/}
+      <ImageBackground source={require('../assets/images/image2.png')}
+        style={styles.imgFundo}>
+
+        <Image source={require('../assets/images/Group5.png')} style={styles.logo}></Image>
+
+      </ImageBackground>
+
+      {/*2. Campo de consulta*/}
+      <View style={styles.container}>
+        {/*2.1 Título*/}
+        <Text style={styles.titulo}>Consulte seu CEP</Text>
+        <Input/>
+
+      </View>
+      {/*2.2 Input*/}
+
+      {/*2.3 Botão*/}
+
+      {/*2.4 Card de informações*/}
+
+    </> //Tag vazia
+  );
+}
+
+//Estilos dos meus componentes
+const styles = StyleSheet.create({
+  imgFundo: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '100%',
+    height: '100%'
+  },
+  logo: {
+    width: 100,
+    height: 120
+  },
+  container: {
+    flex: 1.5,
+    alignItems: "center",
+    paddingTop: 50,
+    paddingBottom: 50,
+    gap: 40
+  },
+  titulo: {
+    fontSize: 25,
+    fontFamily: 'poppins',
+  }
+})
---
+++ findcep/components/input/input.jsx
@@ -0,0 +1,24 @@
+import {Text, View, TextInput, StyleSheet} from "react-native";
+export const Input = () => {
+  return (
+    <>
+      <View style= {styles.camposInput}>
+        <Text style={styles.label}>CEP</Text>
+        <TextInput placeholder="00000-000"  style={styles.input} maxLength={8}></TextInput>
+      </View>
+    </>
+  )
+}
+
+const styles = StyleSheet.create({
+    camposInput: {
+        width: "70%",
+    },
+    input: {
+        borderBottomWidth: 1,
+        borderBottomColor: '#939393',
+        color: '#939393',
+        padding: 5,
+        outlineStyle: 'none'
+    }
+})
